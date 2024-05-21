fd5f051bc78ffc7c9b987aae1de46148b84c0b98
Peternjogu34 <pnjogu364@gmail.com>
2024-05-21 14:02:15 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,48 @@
+from turtle import *
+setup(800,500)
+
+bgcolor("#01411c")
+speed(2)
+
+penup()
+goto(-400, 250)
+pendown()
+color("white")
+begin_fill()
+for i in range(2):
+    forward(200)
+    right(90)
+    forward(500)
+    right(90)
+end_fill()
+
+    #moon shape
+penup()
+goto(50,-135)
+pendown()
+begin_fill()
+circle(150)
+
+end_fill()
+penup()
+goto(90,-80)
+pendown()
+color("#01411c")
+begin_fill()
+circle(130)
+end_fill()
+
+#star
+penup()
+goto(110,90)
+pendown()
+color("white")
+begin_fill()
+for i in range (5):
+    forward(90)
+    left(144)
+end_fill()
+
+
+
+
